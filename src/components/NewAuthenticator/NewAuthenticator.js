b4a1e4c9d39417b9063943a10fc3db294c60a177
Aleksandr Shinkarev <pl.development@nielsen.com>
2021-06-27 17:10:25 +0500

    docs(QR help): #1: update help working, add URL of the github issues page

--- src/components/NewAuthenticator/NewAuthenticator.js
+++ src/components/NewAuthenticator/NewAuthenticator.js
@@ -160,7 +160,16 @@ function ScanView({
           onScan={handleScan}
           style={{ width: '100%' }}
         />
-        <div className="message" style={{ marginTop: '-21px' }}>Point at the QR code</div>
+        <div className="message" style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          paddingLeft: 0,
+          paddingRight: 0
+        }}>
+          Point the camera at the QR code. See Help section if it does not work.
+        </div>
       </div>
       <Softkey
         left="Cancel"
@@ -180,14 +189,22 @@ function HelpView({
   return (
     <>
       <Header title="Help: Scan QR Code" />
-      <div className="content" style={{paddingBottom: '28px'}}>
-        <div className="message" style={{ marginTop: '-27px' }}>
-          Point your device camera at the QR that is going to be used to generate one time password (OTP) codes.
-          <br/>
-          You do not need to push any buttons just wait for a few moments until it is recognized.
-          <br/>
-          In case you accedentally denied access to the camera you can give it another try going to 
-          Settings | Privacy & Security | App Permissions | KaiMFA then select Camera and choose Ask or Grant and re-open the app.
+      <div className="content" style={{ paddingBottom: '28px' }}>
+        <div className="message">
+          Point your device camera at the QR code that is going to be used to generate one time password (OTP) codes. You should put your camera close enough for the QR code to fit in the red frame.
+          <br />
+          You do not need to push any buttons, just wait for a few moments until it is recognized and you are redirected back to the authenticator creation page with the filled Secret field.
+          <br />
+          <br />
+          You can still fill in the secret manually if the QR code recognition does not work, i.e. you are not redirected back to the authenticator creation page after you pointed the camera at the QR code. The Secret or Key is usually available near the QR code itself or hidden under some link like: Can't scan it?
+          <br />
+          If you see the white backgroud and do not see the camera video and you are sure that you allowed the app to access the camera, please close and re-open the app.
+          <br />
+          In case you accidentally denied access to the camera, you can give it another try going to
+          Settings | Privacy & Security | App Permissions | KaiMFA, then select Camera, choose Ask or Grant and re-open the app.
+          <br />
+          <br />
+          In case any issue occurs or if you would like to ask for a new feature in the app, please feel free to open a new issue on the app's GitHub page https://github.com/TourmalineCore/KaiMFA/issues.
         </div>
       </div>
       <Softkey
